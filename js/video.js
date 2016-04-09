870f6383f1b5867c0c492b8918e51e8f121d8ec1
bydesigno <kalmar.skola@gmail.com>
2016-04-09 20:11:37 +0200

    Bootstrap one page
    Bootstrap one page with video background

---
+++ js/video.js
@@ -0,0 +1,16 @@
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
